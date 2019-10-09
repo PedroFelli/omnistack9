ffb9cc32ac854152df6b6985add66d26c3ed758a
PedroFelli <pedrofellipeunai@gmail.com>
2019-10-09 20:57:54 -0300

    login frontend
    login quase pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,14 @@ import './App.css';
 
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+
 function App() {
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
       <div className="content">
-        <p>Ofereça <strong>spots</strong>  para programadores e ecntrontre <strong>talentos</strong> para sua empresa.</p>
-        <form action="">
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail..." />
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
